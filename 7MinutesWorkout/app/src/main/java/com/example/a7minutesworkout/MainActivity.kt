58fe2ae3dac56df13184beb3226f45f1cc3bf19c
Leraedon <thor0249@gmail.com>
2023-09-19 22:25:55 +0900

    lecture 141 to 144

--- 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
                 "Here we sibal",
                 Toast.LENGTH_SHORT
             ).show()
+
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 
