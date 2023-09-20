78b2807eadcc4de58109d38c0264c25d51415764
Leraedon <thor0249@gmail.com>
2023-09-20 15:37:39 +0900

    lecture 153 to 154

---
+++ ViewbindingRV/app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,9 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", "7:00"),
+        Task("Eat BreakFast", "8:00"),
+        Task("Attend a Mitting", "9:00")
+    )
+}
