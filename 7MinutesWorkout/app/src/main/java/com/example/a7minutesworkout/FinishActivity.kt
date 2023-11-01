425e3bbdb21ac5ed509024d4a71ae271b6b8f373
Leraedon <thor0249@gmail.com>
2023-11-01 18:20:11 +0900

    lecture 168 to 178

--- 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -1,8 +1,13 @@
 package com.example.a7minutesworkout
 
+import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -24,6 +29,24 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.btnFinish?.setOnClickListener {
             finish()
+        }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(dao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date: ", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.KOREA)
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date : ", "" + date)
+
+        lifecycleScope.launch{
+            historyDao.insert(HistoryEntity(date))
         }
     }
 }
